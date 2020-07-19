e6b2e9739ce90b269e2e615b8888d5d552343454
Tah Alabyad <tahax09@outlook.de>
2020-07-20 00:36:09 +0200

    added ar.json + style.sass + updated index + added pug.js

---
+++ statics/js/typed.js
@@ -0,0 +1,9 @@
+// typed script
+
+    var typed = new Typed("#typed", {
+        strings:["UI Designer.^1500", "Front-end Developer.^1500", "UX Designer.^1500"],
+        typeSpeed: 50,
+        backSpeed: 50,
+        loop: true,
+        loopCount: Infinity
+    });

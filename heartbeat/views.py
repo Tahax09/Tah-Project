d2509cd5833622f4a5fd70ac397267c36398fb50
ahmed hamoda <aerrre@gmail.com>
2020-08-06 14:32:04 +0200

    - Adding fucking stuff

--- heartbeat/views.py
+++ heartbeat/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 
 # Create your views here.
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpRequest
 from django.utils.translation import activate
 from django.views.generic import TemplateView
 
@@ -15,8 +15,10 @@ def home(request):
 class HomeView(TemplateView):
     template_name = "home.html"
 
-    def get(self, request, *args, **kwargs):
-        activate("ar")
+    def get(self, request: HttpRequest, *args, **kwargs):
+        if "lang" in request.GET.keys():
+            lang = request.GET["lang"]
+            activate(lang)
         return super().get(request, *args, **kwargs)
 
 
